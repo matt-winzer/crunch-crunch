c65028237e8f581ac751eed182961d36b7b9b698
matt-winzer <matt.winzer@gmail.com>
2016-12-02 14:10:00 -0700

    required express and created granola array - DW

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000, function (){
+console.log("working!");
+
+});
+
+
+
+const granolas = [{
+  name: "Dirty Hippie Feet",
+  description: "As crunchy as the crust that forms on a foot that hasnt been washed in a week.",
+  vegan: "Yes",
+  gluten: "No"
+}, {
+  name: "Unshaven Armpit",
+  description: "The smell of this granola will knoock your socks off (in a good way)!",
+  vegan: "Yes",
+  gluten: "Yes"
+}, {
+  name: "Dreadlock and Hoola Hoops",
+  description: "So delicious you feel tripping acid at a Phish show!",
+  vegan: "Yes",
+  gluten: "Yes"
+}]
